a8a2bbdcf3fcd6c9e4dbc3975eabda8a34454c53
Sora-mmh <montassarmhamdi80@gmail.com>
2024-04-11 16:02:04 +0200

    call mistral-llm, mistral tokenizer

---
+++ builder/_base.py
@@ -0,0 +1,24 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import torch
+
+import config._base as cfg
+
+
+class Builder:
+    def __init__(self, model_name: str = cfg.model_name) -> None:
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=cfg.bnb_params["use_4bit"],
+            bnb_4bit_quant_type=cfg.bnb_params["bnb_4bit_quant_type"],
+            bnb_4bit_compute_dtype=getattr(
+                torch, cfg.bnb_params["bnb_4bit_quant_dtype"]
+            ),
+            bnb_4bit_use_double_quant=cfg.bnb_params["use_nested_quant"],
+        )
+        self._baseline_model = AutoModelForCausalLM.from_pretrained(
+            model_name, quantization_config=bnb_config, device_map=cfg.device_map
+        )
+        self._baseline_model.config.use_cache = False
+        self._baseline_model.config.pretraining_tp = 1
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.padding_side = "right"
